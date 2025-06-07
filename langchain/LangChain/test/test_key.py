67dc76808d0d428b06be1dc578453772e19b956e
llch <aceliuchanghong@gmail.com>
2025-06-07 22:43:18 +0800

    feat: 让模型扮演求职者,狠狠拷打他

--- langchain/LangChain/test/test_key.py
+++
@@ -1,8 +0,0 @@
-import os
-import getpass
-
-PROMPTLAYER_API_KEY = getpass.getpass()
-os.environ["LANGCHAIN_API_KEY"] = PROMPTLAYER_API_KEY
-
-OPENAI_API_KEY = getpass.getpass()
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
